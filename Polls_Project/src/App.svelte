4d3600be48f2849490bd03ba9d4ec9cac0f821db
Liam Spillane <123liamspillane@gmail.com>
2024-01-11 03:37:51 -0500

    through tutorial video 25

--- Polls_Project/src/App.svelte
+++ Polls_Project/src/App.svelte
@@ -1,9 +1,52 @@
 <script>
+	import Header from "./components/Header.svelte";
+	import Tabs from "./shared/Tabs.svelte";
+	import PollList from "./components/PollList.svelte";
+	import CreatePollForm from "./components/CreatePollForm.svelte";
+	import Footer from "./components/Footer.svelte";
+
+	// tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
+
+	// polls
+	let polls = [
+		{
+			id: 1,
+			question: 'Python or JavaScript?',
+			answerA: 'Python',
+			answerB: 'JavaScript',
+			votesA: 9,
+			votesB: 15,
+		},
+	];
+
+	const handleAdd = (e) => {
+		const poll = e.detail;
+		polls = [...polls, poll];
+		console.log(polls);
+		activeItem = 'Current Polls';
+	}
 </script>
 
+<Header />
 <main>
-	<h1>Hello Ninjas!</h1>
+	<Tabs {activeItem} {items} on:tabChange={tabChange} />
+	{#if activeItem === 'Current Polls'}
+		<PollList {polls} />
+	{:else if activeItem === 'Add New Poll'}
+		<CreatePollForm on:add={handleAdd} />
+	{/if}
 </main>
+<Footer />
 
 <style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
 </style>
